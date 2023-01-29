669cf1baa2bd358a96481937fe1fc350be0d49ae
andri (arch) <andri.baal@bluewin.ch>
2023-01-29 16:56:11 +0100

    temp

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 use proc_macro::TokenStream;
-use syn::__private::ToTokens;
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Type, __private::ToTokens};
 
-fn position_field(data_struct: &syn::DataStruct, attr_name: &str) -> Option<(String, String)> {
+fn position_field(data_struct: &DataStruct, attr_name: &str) -> Option<(String, String)> {
     match data_struct.fields {
-        syn::Fields::Named(ref fields_named) => {
+        Fields::Named(ref fields_named) => {
             for field in fields_named.named.iter() {
                 for attr in &field.attrs {
                     let name = attr.path.to_token_stream().to_string();
                     if name == attr_name {
                         match field.ty {
-                            syn::Type::Path(ref p) => {
+                            Type::Path(ref p) => {
                                 let field_name = field.ident.as_ref().unwrap();
                                 let type_name = p.to_token_stream().to_string();
                                 return Some((field_name.to_string(), type_name.to_string()));
@@ -38,11 +38,11 @@ fn position_field(data_struct: &syn::DataStruct, attr_name: &str) -> Option<(Str
 /// }
 /// ```
 pub fn derive_component(input: TokenStream) -> TokenStream {
-    let ast = syn::parse_macro_input!(input as syn::DeriveInput);
+    let ast = parse_macro_input!(input as DeriveInput);
     let struct_name = ast.ident;
 
     let data_struct = match ast.data {
-        syn::Data::Struct(ref data_struct) => data_struct,
+        Data::Struct(ref data_struct) => data_struct,
         _ => panic!("Must be a struct!"),
     };
 
@@ -51,12 +51,12 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
 
     format!(
         "
-impl ComponentDerive for {struct_name} {{
-    fn inner(&self) -> &dyn shura::BaseComponent {{
+impl SceneDerive for {struct_name} {{
+    fn inner(&self) -> &dyn shura::BaseScene {{
         &self.{field_name}
     }}
 
-    fn inner_mut(&mut self) -> &mut dyn shura::BaseComponent {{
+    fn inner_mut(&mut self) -> &mut dyn shura::BaseScene {{
         &mut self.{field_name}
     }}
 }}
@@ -79,8 +79,6 @@ impl std::ops::DerefMut for {struct_name} {{
     .unwrap()
 }
 
-
-
 #[proc_macro_derive(Scene, attributes(scene))]
 pub fn derive_scene(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
@@ -96,9 +94,30 @@ pub fn derive_scene(input: TokenStream) -> TokenStream {
 
     format!(
         "
+impl ComponentDerive for {struct_name} {{
+    fn inner(&self) -> &dyn shura::BaseScene {{
+        &self.{field_name}
+    }}
+
+    fn inner_mut(&mut self) -> &mut dyn shura::BaseScene {{
+        &mut self.{field_name}
+    }}
+}}
+
+impl std::ops::Deref for {struct_name} {{
+    type Target = {type_name};
+    fn deref(&self) -> &Self::Target {{
+        &self.{field_name}
+    }}
+}}
+
+impl std::ops::DerefMut for {struct_name} {{
+    fn deref_mut(&mut self) -> &mut Self::Target {{
+        &mut self.{field_name}
+    }}
+}}
 ",
     )
     .parse()
     .unwrap()
 }
-
