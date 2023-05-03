e00f26c077410543a09fe690b7fbf9a504a044d0
Andri <andri.baal@bluewin.ch>
2023-05-03 19:22:34 +0200

    temp

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro2::{Ident, TokenStream as TokenStream2};
 use quote::{quote, ToTokens};
 use std::sync::Mutex;
 use std::{collections::HashSet, str::FromStr};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Type, TypePath};
+use syn::{parse_macro_input, parse_quote, Data, DataStruct, DeriveInput, Fields, Type, TypePath};
 
 fn field_names(data_struct: &DataStruct) -> Vec<String> {
     let mut result = vec![];
@@ -47,25 +47,24 @@ fn position_field(data_struct: &DataStruct, attr_name: &str) -> Option<(Ident, T
     None
 }
 
-fn name_field(ast: &DeriveInput, attr_name: &str) -> Option<String> {
+fn name_field(ast: &DeriveInput, attr_name: &str) -> Option<syn::Expr> {
     for attr in &ast.attrs {
-        let name = attr.path().to_token_stream().to_string();
-        if name == attr_name {
-            let mut value = attr.to_token_stream().to_string();
-            assert!(
-                value.len() >= 3,
-                "You need to provide a name like the following pattern: '#[name(Bunny)]'!"
-            );
-            value.pop();
-            value.remove(0);
-            return Some(value);
+        let name = match &attr.meta {
+            syn::Meta::NameValue(p) => p,
+            _ => continue,
+        };
+
+        let name_str = attr.path().to_token_stream().to_string();
+        if name_str == attr_name {
+            return Some(name.value.clone());
         }
     }
     return None;
 }
 
 lazy_static! {
-    static ref USED_HASHES: Mutex<HashSet<u32>> = Mutex::new(HashSet::new());
+    static ref USED_NAME_HASHES: Mutex<HashSet<u32>> = Mutex::new(HashSet::new());
+    static ref USED_STATE_HASHES: Mutex<HashSet<u32>> = Mutex::new(HashSet::new());
 }
 
 #[proc_macro_derive(Component, attributes(base, name))]
@@ -87,18 +86,20 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
     };
 
     let struct_name = ast.ident.clone();
-    let struct_identifier = name_field(&ast, "name").unwrap_or(struct_name.to_string().clone());
+    let struct_name_str = struct_name.to_string();
+    let struct_identifier = name_field(&ast, "name").unwrap_or(parse_quote!(#struct_name_str));
+    let struct_identifier_str = struct_identifier.to_token_stream().to_string();
     let fields =
         TokenStream2::from_str(&format!("&[{}]", field_names(data_struct).join(", "))).unwrap();
 
     let (field_name, _) = position_field(data_struct, "base")
         .expect("The helper attribute #[component] has not been found!");
 
-    let mut hashes = USED_HASHES.lock().unwrap();
-    let hash = const_fnv1a_hash::fnv1a_hash_str_32(&struct_identifier);
+    let mut hashes = USED_NAME_HASHES.lock().unwrap();
+    let hash = const_fnv1a_hash::fnv1a_hash_str_32(&struct_identifier_str);
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
     if hashes.contains(&hash) {
-        panic!("A component with the struct identifier '{struct_identifier}' already exists!");
+        panic!("A component with the identifier '{struct_identifier_str}' already exists!");
     }
     hashes.insert(hash);
 
@@ -143,10 +144,22 @@ pub fn derive_component(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(State)]
 pub fn derive_state(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    let data_struct = match ast.data {
+    let data_struct = &match ast.data {
         Data::Struct(ref data_struct) => data_struct,
         _ => panic!("Must be a struct!"),
     };
+
+    let struct_name = ast.ident.clone();
+    let struct_name_str = struct_name.to_string();
+    let struct_identifier = name_field(&ast, "name").unwrap_or(parse_quote!(#struct_name_str));
+    let struct_identifier_str = struct_identifier.to_token_stream().to_string();
+    let priority = name_field(&ast, "priority").unwrap_or(parse_quote!(0));
+    let mut hashes = USED_NAME_HASHES.lock().unwrap();
+    let hash = const_fnv1a_hash::fnv1a_hash_str_32(&struct_identifier_str);
+    if hashes.contains(&hash) {
+        panic!("A state with the identifier '{struct_identifier_str}' already exists!");
+    }
+    hashes.insert(hash);
 
     let struct_name = ast.ident;
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
@@ -155,6 +168,9 @@ pub fn derive_state(input: TokenStream) -> TokenStream {
 
     quote!(
         impl #impl_generics State for #struct_name #ty_generics #where_clause {
+            const TYPE_NAME: &'static str = #struct_identifier;
+            const IDENTIFIER: shura::StateTypeId = shura::StateTypeId::new(#hash);
+            const PRIORITY: i16 = #priority;
         }
 
         impl #impl_generics shura::FieldNames for #struct_name #ty_generics #where_clause {
